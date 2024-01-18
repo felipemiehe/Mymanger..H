bf2c08d42d3b108bf4117be032bf87adde89a6ec
Felipe Miehe <felipemiehe555@gmail.com>
2024-01-18 19:19:12 -0300

    arrumado detalhes de experiencia do usuario

--- front/Models/UserXUserViewModel.cs
+++ front/Models/UserXUserViewModel.cs
@@ -7,5 +7,10 @@ namespace front.Models
         public UserXUserViewModel(int id, string userId, string email, string nome, string numero, string cpf, string codigoUnico, List<string> roles, int totalAchados, int totalPages) : base(id, userId, email, nome, numero, cpf, codigoUnico, roles, totalAchados, totalPages)
         {
         }
+
+        public string CodigoUnicoExibicao
+        {
+            get { return CodigoUnico.Length > 10 ? CodigoUnico.Substring(0, 10) + "..." : CodigoUnico; }
+        }
     }
 }
